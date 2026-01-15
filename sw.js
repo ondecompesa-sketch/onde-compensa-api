b0ec36222701d7c284ad1e866923a047f658f3ba
Eduardo Bezerra <eduardo@newit.com.br>
2026-01-15 16:08:31 -0300

    icon3

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ondecompensa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './login.html',
+  './nova-nota.html',
+  './perfil.html',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
